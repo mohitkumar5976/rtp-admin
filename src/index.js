c43117dadb500b093b2f36bd81ab9f7cf91876db
Mohit Kumar <86621804+mohitkumar5976@users.noreply.github.com>
2024-02-21 03:16:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import ContextProvider from "./context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ContextProvider>
     <App />
-  </ContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
